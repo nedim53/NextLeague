9237e796d4d624608a5a026f151294190b4ce03a
muminovixc <edin.muminovic@student.pmf.unsa.ba>
2026-02-20 18:24:08 +0100

    Refactor login redirection in LoginPage component to use router.push for improved navigation handling and refresh functionality.

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -18,8 +18,8 @@ export default function LoginPage() {
       const data = await loginUser(email, password);
       console.log('Login successful:', data);
 
-      // npr. redirectuj na dashboard ili home
-      location.assign('/homepage');
+      router.push('/homepage');
+      router.refresh();
     } catch (err) {
       setError(err.message);
     }
