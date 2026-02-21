7e6d2d0aa508f727901632bfb3b9dd5da5f401cf
muminovixc <edin.muminovic@student.pmf.unsa.ba>
2026-02-21 16:21:39 +0100

    Update login redirection in LoginPage component to use window.location.href and location.assign for improved navigation consistency.

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -18,7 +18,8 @@ export default function LoginPage() {
       const data = await loginUser(email, password);
       console.log('Login successful:', data);
 
-      router.push("/homepage");
+      window.location.href('/homepage');
+      location.assign('/homepage');
     } catch (err) {
       setError(err.message);
     }
