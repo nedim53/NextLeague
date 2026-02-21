44f8e43963fe6fed5ed91b4182b1c1273b6c1ec7
muminovixc <edin.muminovic@student.pmf.unsa.ba>
2026-02-21 16:39:03 +0100

    .

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -18,8 +18,8 @@ export default function LoginPage() {
       const data = await loginUser(email, password);
       console.log('Login successful:', data);
 
-      window.location.href='/homepage';
-      
+     
+      location.assign('/homepage');
     } catch (err) {
       setError(err.message);
     }
